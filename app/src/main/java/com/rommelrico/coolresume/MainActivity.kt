7d85c1c10b6fbd0a0f0d6a59e197d2b275d65c05
Rommel Rico <me@rommelrico.com>
2018-08-29 19:37:27 -0700

    setting an OnClickListener on the workHistory button

--- app/src/main/java/com/rommelrico/coolresume/MainActivity.kt
+++ app/src/main/java/com/rommelrico/coolresume/MainActivity.kt
@@ -2,11 +2,17 @@ package com.rommelrico.coolresume
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var workHistory = findViewById<Button>(R.id.workHistoryButton)
+        workHistory.setOnClickListener {
+            
+        }
     }
 }
